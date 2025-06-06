737dd155b7c993f0c238e350a91d8da48d79f688
Sam H <samuelh@missionreadyhq.com>
2025-06-06 20:30:11 +1200

    feat: :sparkles: add @flow-budget/database dependency and update server routes

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { Person } from "@flow-budget/database";
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,13 @@ const port = 3000;
 app.use(cors());
 
 app.get("/budget", (req: express.Request, res: express.Response) => {
-    console.log("Received request for budget data");
-  res.json({april: 1000, may: 1200, june: 800});
+  console.log("Received request for budget data");
+  res.json({ april: 1000, may: 1200, june: 800 });
+});
+
+app.get("/person", (req: express.Request, res: express.Response) => {
+  console.log("Received request for person data");
+  res.json({});
 });
 
 app.listen(port, () => {
